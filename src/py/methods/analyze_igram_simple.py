getting old results on interactive notebook. fixed script too. bug was reading degrees and not convering to radians.

src/py/methods/analyze_igram_simple.py
@@ -194,15 +194,13 @@ def compute_phase_offset(
     return phase_diff
     
 # phase computed of alos1 - alos2, so  must be computed this way too
-incidence_angle = 38.7*np.pi/180
-wavelength = 0.2360571
 with open(alos_isce_outputs_dir / "PICKLE/interferogram", "rb") as fp:
     pickle_isce_obj = pickle.load(fp)
         
-radar_wavelength = pickle_isce_obj['reference']['instrument']['radar_wavelength']
+wavelength = pickle_isce_obj['reference']['instrument']['radar_wavelength']
 incidence_angle = pickle_isce_obj['reference']['instrument']['incidence_angle']
 
-print('radar wavelength', radar_wavelength)
+print('radar wavelength', wavelength)
 print('incidence angle', incidence_angle)
 
 phase_corr = compute_phase_offset(

src/py/methods/schaefer.py
@@ -172,7 +172,10 @@ def process_scene_pair(alos1, alos2, df_calm_points, calib_point_id, df_temp):
     with open(isce_output_dir / "PICKLE/interferogram", "rb") as fp:
         pickle_isce_obj = pickle.load(fp)
     radar_wavelength = pickle_isce_obj['reference']['instrument']['radar_wavelength']
-    incidence_angle = pickle_isce_obj['reference']['instrument']['incidence_angle']
+    incidence_angle = pickle_isce_obj['reference']['instrument']['incidence_angle'] * np.pi/180
+
+    print("radar wavelength:", radar_wavelength)
+    print("incidence angle:", incidence_angle)
 
     point_to_pixel = []
     for (i, row) in tqdm.tqdm(df_calm_points.iterrows(), total=len(df_calm_points)):