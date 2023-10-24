96154f02d20fa5d330b5056b9ffdaae4cfb9579d
Jatin Mathur <jatin.mathur25@gmail.com>
2023-10-23 18:33:21 -0700

    if we use a single calibration point (like Liu), we do significantly better. but node 61 has a lot of nans because in 2006 it is on the lower end, with an alt of 21cm when the avg is 31cm. This causes my method to be like 40% nans. If I shift to node 1cm, my method gets 12cm of error with 5 nans, while Liu blows up to 60cm! Not sure why, should look into it. The point is, is showing promise. I would basically call it a self-consistent ReSALT.

--- src/py/methods/jatin_ddt_fit.py
+++ src/py/methods/jatin_ddt_fit.py
@@ -67,6 +67,13 @@ year = get_unique(df_alt_gt, 'year')
 month = get_unique(df_alt_gt, 'month')
 day = get_unique(df_alt_gt, 'day')
 
+# print("RENORMALIZING TO MEASUREMENT")
+# measurement_ddt = df_temp.loc[year, month, day]['norm_ddt']
+# df_temp['norm_ddt'] = df_temp['norm_ddt'] / measurement_ddt
+# df_temp[df_temp['norm_ddt'] > 1.0] = 1.0
+# df_alt_gt['norm_ddt'] = df_alt_gt['norm_ddt'] / measurement_ddt
+# df_alt_gt[df_alt_gt['norm_ddt'] > 1.0] = 1.0
+
 si = igrams_usable
 n = len(si)
 lhs_all = np.zeros((n, len(df_alt_gt)))
@@ -86,14 +93,26 @@ print("Calibration date, scene:", calib_date, calib_scene)
 
 avg_alt_calib = df_alt_gt['alt_m'].mean()
 avg_sqrt_norm_ddt_calib = np.sqrt(df_alt_gt['norm_ddt'].values).mean()
+calib_point_id = 1
+use_calib_node = True
+
+if use_calib_node:
+    print("Calibrating using calibration node")
+else:
+    print("Calibrating using average")
 
 subs = df_alt_gt['alt_m'].apply(alt_to_surface_deformation)
 avg_subsidence_stddev = np.std(subs)
 
-def get_scene_expected_alt(scene):
+def get_scene_expected_alt(scene, use_calib_node: bool):
     scene_d = get_date_for_alos(scene)[1]
     scene_sqrt_ddt = np.sqrt(df_temp.loc[scene_d.year, scene_d.month, scene_d.day]['norm_ddt'])
-    avg_scene_alt = avg_alt_calib * scene_sqrt_ddt / avg_sqrt_norm_ddt_calib
+    if use_calib_node:
+        calib_sqrt_ddt = np.sqrt(df_alt_gt.loc[calib_point_id]['norm_ddt'])
+        calib_alt = df_alt_gt.loc[calib_point_id]['alt_m']
+        avg_scene_alt = calib_alt * scene_sqrt_ddt / calib_sqrt_ddt
+    else:
+        avg_scene_alt = avg_alt_calib * scene_sqrt_ddt / avg_sqrt_norm_ddt_calib
     return avg_scene_alt
 
 def get_expected_alt_per_pixel(scene):
@@ -108,8 +127,8 @@ rhs_all = np.zeros((n, 2))
 for i, (scene1, scene2) in enumerate(si):
     if needs_sign_flip:
         scene2, scene1 = scene1, scene2
-    scene1_avg_alt = get_scene_expected_alt(scene1)
-    scene2_avg_alt = get_scene_expected_alt(scene2)
+    scene1_avg_alt = get_scene_expected_alt(scene1, use_calib_node)
+    scene2_avg_alt = get_scene_expected_alt(scene2, use_calib_node)
     scene1_avg_def = alt_to_surface_deformation(scene1_avg_alt)
     scene2_avg_def = alt_to_surface_deformation(scene2_avg_alt)
     expected_avg_def = scene1_avg_def - scene2_avg_def
@@ -134,7 +153,14 @@ for i, (scene1, scene2) in enumerate(si):
         needs_sign_flip=needs_sign_flip,
         # ideal_deformation=expected_deformation_per_pixel
     )
-    deformation_per_pixel = deformation_per_pixel - deformation_per_pixel.mean() + expected_avg_def
+    if use_calib_node:
+        matches = np.argwhere(df_alt_gt.index==calib_point_id)
+        assert matches.shape == (1,1)
+        # The delta needed to make the calibration node have the right deformation
+        calib_node_delta = expected_avg_def - deformation_per_pixel[matches[0,0]]
+        deformation_per_pixel = deformation_per_pixel + calib_node_delta
+    else:
+        deformation_per_pixel = deformation_per_pixel - deformation_per_pixel.mean() + expected_avg_def
     # lhs = np.sqrt(2*expected_N_per_pixel) * rhs[1]
     scene1_avg_def = alt_to_surface_deformation(scene1_avg_alt)
     scene2_est_deformation_per_pixel = scene1_avg_def - deformation_per_pixel
@@ -157,7 +183,7 @@ alt_pred = sol[0, :]
 avg_sqrt_ddt = np.sqrt(df_alt_gt['norm_ddt'].values).mean()
 alt_pred = alt_pred * avg_sqrt_ddt
 
-print("AVG ALT", np.mean(alt_pred))
+print("AVG ALT", np.nanmean(alt_pred))
 
 alt_gt = df_alt_gt["alt_m"].values
 compute_stats(alt_pred, alt_gt)
